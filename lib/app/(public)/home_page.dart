a59f1caf542422c5fcd1c4537425e736251f036d
The-vinicuis <viniciusnao41@gmail.com>
2024-02-18 08:19:00 -0300

    list muscles workout

--- lib/app/(public)/home_page.dart
+++ lib/app/(public)/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:registry_pull/app/core/widgets/muscle_title.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,11 +7,47 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Treinos'),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/gym3.jpg'), fit: BoxFit.cover),
+        ),
+        child: const Column(
+          children: [
+            Text(
+              'Treinos',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 30,
+                color: Colors.white70,
+                shadows: [
+                  Shadow(
+                      color: Colors.black87,
+                      offset: Offset(0, 2),
+                      blurRadius: 4),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            MuscleTitle(title: 'Braços'),
+            SizedBox(
+              height: 20,
+            ),
+            MuscleTitle(title: 'Peito'),
+            SizedBox(
+              height: 20,
+            ),
+            MuscleTitle(title: 'Pernas'),
+            SizedBox(
+              height: 20,
+            ),
+            MuscleTitle(title: 'Costas'),
+          ],
+        ),
       ),
-      body: const Center(child: Text('home')),
     );
   }
 }
