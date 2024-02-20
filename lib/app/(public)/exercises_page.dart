5d9aa16abe8b29efa9c6b50af9a23826b27ef609
The-vinicuis <viniciusnao41@gmail.com>
2024-02-20 09:47:20 -0300

    exercies steps

--- lib/app/(public)/exercises_page.dart
+++ lib/app/(public)/exercises_page.dart
@@ -29,6 +29,7 @@ class _ExercisesPageState extends State<ExercisesPage> {
             color: Colors.black,
           ),
         ),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
       ),
       body: SingleChildScrollView(
         child: ExpansionPanelList(
@@ -41,11 +42,135 @@ class _ExercisesPageState extends State<ExercisesPage> {
               ExpansionPanel(
                 headerBuilder: (context, ex) {
                   return const ListTile(
-                    title: Text('sadsa'),
+                    title: Text('Paralela'),
                   );
                 },
-                body: const ListTile(
-                  title: Text('sadsa'),
+                body: ListTile(
+                  title: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Data',
+                          textAlign: TextAlign.left,
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(5),
+                              margin: const EdgeInsets.only(right: 5),
+                              decoration: const BoxDecoration(
+                                  color: Colors.lightBlueAccent,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10))),
+                              width: 60,
+                              height: 30,
+                              child: const Text(
+                                '20/02',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(5),
+                              margin: const EdgeInsets.only(right: 5),
+                              decoration: const BoxDecoration(
+                                  color: Colors.lightBlueAccent,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10))),
+                              width: 60,
+                              height: 30,
+                              child: const Text(
+                                '20/02',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.add_circle_outline))
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text(
+                          'Series',
+                          textAlign: TextAlign.left,
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(5),
+                              margin: const EdgeInsets.only(right: 5),
+                              decoration: const BoxDecoration(
+                                  color: Colors.lightBlueAccent,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10))),
+                              width: 30,
+                              height: 30,
+                              child: const Text(
+                                '1',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(5),
+                              margin: const EdgeInsets.only(right: 5),
+                              decoration: const BoxDecoration(
+                                  color: Colors.lightBlueAccent,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10))),
+                              width: 30,
+                              height: 30,
+                              child: const Text(
+                                '2',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text('Repetições: 5'),
+                        Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(right: 5),
+                              height: 50,
+                              width: 10,
+                              color: Colors.red,
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(right: 5),
+                              height: 50,
+                              width: 10,
+                              color: Colors.red,
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(right: 5),
+                              height: 50,
+                              width: 10,
+                              color: Colors.red,
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(right: 5),
+                              height: 50,
+                              width: 10,
+                              color: Colors.red,
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(right: 5),
+                              height: 50,
+                              width: 10,
+                              color: Colors.red,
+                            ),
+                          ],
+                        ),
+                      ]),
                 ),
                 isExpanded: expan,
               ),
