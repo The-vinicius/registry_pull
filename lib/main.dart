628e1160741965bc681dc0cca233ca2ece159412
The-vinicuis <viniciusnao41@gmail.com>
2024-02-29 08:09:06 -0300

    register injector

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:registry_pull/app/app_registry.dart';
+import 'package:registry_pull/app/injector.dart';
 
 void main() {
+  registerInstances();
   runApp(const AppRegistry());
 }
