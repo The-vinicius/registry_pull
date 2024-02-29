2fc8cf5ef290dd8eeb0c7d96df321a03cfc84efc
The-vinicuis <viniciusnao41@gmail.com>
2024-02-29 08:07:44 -0300

    actions exercises

---
+++ lib/app/interactor/actions/exercises_action.dart
@@ -0,0 +1,23 @@
+import 'package:registry_pull/app/injector.dart';
+import 'package:registry_pull/app/interactor/atoms/exercise_atom.dart';
+import 'package:registry_pull/app/interactor/models/exercises_model.dart';
+import 'package:registry_pull/app/interactor/repositories/exercises_repository.dart';
+
+Future<void> putExercises(ExercisesModel model) async {
+  final repository = injector.get<ExercisesRepository>();
+  await repository.insertExercise(model);
+  await getExercises(model.nameMuscle);
+}
+
+Future<void> getExercises(String muscle) async {
+  loading.value = true;
+  final repository = injector.get<ExercisesRepository>();
+  exerciseState.value = await repository.getExercises(muscle);
+  loading.value = false;
+}
+
+deleteExercise(String id, String muscle) async {
+  final repository = injector.get<ExercisesRepository>();
+  await repository.deleteExercise(id, muscle);
+  await getExercises(muscle);
+}
