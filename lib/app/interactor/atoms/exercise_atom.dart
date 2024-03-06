a71f3228aab93275897e2398e82c609e24950497
The-vinicuis <viniciusnao41@gmail.com>
2024-03-06 08:12:17 -0300

    rm atoms

--- lib/app/interactor/atoms/exercise_atom.dart
+++ lib/app/interactor/atoms/exercise_atom.dart
@@ -1,22 +1,5 @@
 import 'package:asp/asp.dart';
-import 'package:flutter/material.dart';
 import 'package:registry_pull/app/interactor/models/exercises_model.dart';
 
 final exerciseState = Atom<List<ExercisesModel>>([]);
 final loading = Atom<bool>(true);
-
-class Days extends ValueNotifier<int> {
-  Days(int value) : super(value);
-
-  void increment() {
-    notifyListeners();
-  }
-}
-
-class SeriesIndex extends ValueNotifier<int> {
-  SeriesIndex(int value) : super(value);
-
-  void increment() {
-    notifyListeners();
-  }
-}
