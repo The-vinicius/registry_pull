ca46e9e6f9081596c118cf41f16c17e87d2389dc
The-vinicuis <viniciusnao41@gmail.com>
2024-07-14 09:11:02 -0300

    update list day color index

--- lib/app/core/widgets/list_date.dart
+++ lib/app/core/widgets/list_date.dart
@@ -2,22 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:registry_pull/app/interactor/models/day_exercise_model.dart';
 import 'package:brasil_datetime/brasil_datetime.dart' as data;
 
-Widget listDate(DayExerciseModel? day, int index, Function(int, bool) toggle) {
+Widget listDate(
+    DayExerciseModel? day, int index, Function(int, bool) toggle, indexId) {
   return GestureDetector(
     onTap: () {
-      toggle(day.id, true);
+      toggle(indexId, true);
     },
     child: Container(
       padding: const EdgeInsets.all(5),
       margin: const EdgeInsets.only(right: 5),
       decoration: BoxDecoration(
-          color: index == day!.id ? Colors.orange : Colors.lightBlueAccent,
+          color: index == indexId ? Colors.orange : Colors.lightBlueAccent,
           borderRadius: const BorderRadius.all(Radius.circular(10))),
       width: 80,
       height: 30,
       child: Center(
         child: Text(
-          day.date.diaMesAbrev(),
+          day!.date.diaMesAbrev(),
           textAlign: TextAlign.center,
           style: const TextStyle(color: Colors.white),
         ),
