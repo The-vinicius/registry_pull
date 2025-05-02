672727a061ca857cef4bf085734c5597d1ba1b9e
The-vinicuis <viniciusnao41@gmail.com>
2025-05-02 16:09:57 -0300

    fix

--- lib/app/injector.dart
+++ lib/app/injector.dart
@@ -1,7 +1,6 @@
 import 'package:auto_injector/auto_injector.dart';
 import 'package:registry_pull/app/data/repositories/localstore_exercises_repository.dart';
 import 'package:registry_pull/app/interactor/repositories/exercises_repository.dart';
-import 'package:registry_pull/app/interactor/repositories/pull_repository.dart';
 
 final injector = AutoInjector();
 
