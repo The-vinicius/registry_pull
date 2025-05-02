07e5bc7594812fc136d3ea22983a3546255dbc66
The-vinicuis <viniciusnao41@gmail.com>
2025-05-02 15:55:16 -0300

    Update dependency injection to use LocalstoreExercisesRepository for exercises management

--- lib/app/injector.dart
+++ lib/app/injector.dart
@@ -1,10 +1,11 @@
 import 'package:auto_injector/auto_injector.dart';
-import 'package:registry_pull/app/data/repositories/sqlite_repository_impl.dart';
+import 'package:registry_pull/app/data/repositories/localstore_exercises_repository.dart';
+import 'package:registry_pull/app/interactor/repositories/exercises_repository.dart';
 import 'package:registry_pull/app/interactor/repositories/pull_repository.dart';
 
 final injector = AutoInjector();
 
 void registerInstances() {
-  injector.add<PullRepository>(SqliteRepositoryImpl.new);
+  injector.add<ExercisesRepository>(LocalstoreExercisesRepository.new);
   injector.commit();
 }
